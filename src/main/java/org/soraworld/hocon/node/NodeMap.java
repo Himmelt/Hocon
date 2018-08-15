a9a22e8e4f842d083fd57c6a311c35914aa78324
Himmelt <master@void-3.cn>
2018-08-15 23:21:12 +0800

    update

--- src/main/java/org/soraworld/hocon/node/NodeMap.java
+++ src/main/java/org/soraworld/hocon/node/NodeMap.java
@@ -118,7 +118,7 @@ public class NodeMap implements Node {
         }
     }
 
-    private Node getNode(String path) {
+    public Node getNode(String path) {
         return value.get(path);
     }
 
