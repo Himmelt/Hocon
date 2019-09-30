4770972dbfde23ad1004a4de4b4cbf392c490f04
Himmelt <master@void-3.cn>
2019-09-30 23:58:53 +0800

    modify MapSerializer

--- src/main/java/org/soraworld/hocon/serializer/MapSerializer.java
+++ src/main/java/org/soraworld/hocon/serializer/MapSerializer.java
@@ -25,8 +25,8 @@ final class MapSerializer extends TypeSerializer<Map<?, ?>, NodeMap> {
     MapSerializer() throws SerializerException {
     }
 
-    @NotNull
-    public Map<?, ?> deserialize(@NotNull Type fieldType, @NotNull NodeMap node) throws HoconException {
+    @Override
+    public @NotNull Map<?, ?> deserialize(@NotNull Type fieldType, @NotNull NodeMap node) throws HoconException {
         Type[] arguments = Reflects.getActualTypes(Map.class, fieldType);
         if (arguments != null && arguments.length == 2) {
             Options options = node.options();
@@ -37,7 +37,9 @@ final class MapSerializer extends TypeSerializer<Map<?, ?>, NodeMap> {
                 map = (Map) getTypeInstance(fieldType);
             } catch (Throwable e) {
                 System.out.println("Failed to construct instance for " + fieldType.getTypeName() + " , will fall back to LinkedHashMap.");
-                if (options.isDebug()) e.printStackTrace();
+                if (options.isDebug()) {
+                    e.printStackTrace();
+                }
                 map = new LinkedHashMap<>();
             }
             for (String path : node.keys()) {
@@ -46,29 +48,39 @@ final class MapSerializer extends TypeSerializer<Map<?, ?>, NodeMap> {
                 map.put(key, val);
             }
             return map;
-        } else throw new NotMatchException(getType(), fieldType);
+        } else {
+            throw new NotMatchException(getType(), fieldType);
+        }
     }
 
-    @NotNull
-    public NodeMap serialize(@NotNull Type fieldType, @NotNull Map<?, ?> value, @NotNull Options options) throws HoconException {
+    @Override
+    public @NotNull NodeMap serialize(@NotNull Type fieldType, @NotNull Map<?, ?> value, @NotNull Options options) throws HoconException {
         Type[] arguments = Reflects.getActualTypes(Map.class, fieldType);
         if (arguments != null && arguments.length == 2) {
             NodeMap node = new NodeMap(options);
-            TypeSerializer KEY = options.getSerializer(arguments[0]);
-            TypeSerializer VAL = options.getSerializer(arguments[1]);
-            if (!value.isEmpty() && KEY.keyAble()) {
+            if (!value.isEmpty()) {
+                final TypeSerializer KEY_S = options.getSerializer(arguments[0]);
+                final TypeSerializer VAL_S = options.getSerializer(arguments[1]);
                 for (Map.Entry<?, ?> entry : value.entrySet()) {
                     Object key = entry.getKey();
                     Object obj = entry.getValue();
-                    if (key != null && obj != null) {
-                        NodeBase base = (NodeBase) KEY.serialize(arguments[0], key, options);
-                        if (!node.add(base.getString(), VAL.serialize(arguments[1], obj, options))) {
-                            throw new SerializerException("Node for key <" + base.getString() + "> already exist !");
+                    TypeSerializer KEY = KEY_S != null ? KEY_S : options.getSerializer(key.getClass());
+                    TypeSerializer VAL = VAL_S != null ? VAL_S : options.getSerializer(obj.getClass());
+                    if (KEY != null && VAL != null) {
+                        if (KEY.keyAble() && key != null && obj != null) {
+                            NodeBase base = (NodeBase) KEY.serialize(arguments[0], key, options);
+                            if (!node.add(base.getString(), VAL.serialize(arguments[1], obj, options))) {
+                                throw new SerializerException("Node for key <" + base.getString() + "> already exist !");
+                            }
                         }
+                    } else {
+                        throw new HoconException("KEY type " + key.getClass() + " has no serializer !!!");
                     }
                 }
             }
             return node;
-        } else throw new NotMatchException(getType(), fieldType);
+        } else {
+            throw new NotMatchException(getType(), fieldType);
+        }
     }
 }
